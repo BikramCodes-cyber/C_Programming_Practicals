ccfc24d7b167edfae5fe1e836aeff456d4a2d3dc
BikramCodes-cyber <kumarbikram13924@gmail.com>
2026-07-25 13:31:01 +0530

    Keep only q1.c

--- q8.c
+++
@@ -1,9 +0,0 @@
-#include<stdio.h>
- int main ()
- {
-    float length = 10.5, width = 4.5, area, perimeter;
-    area = length * width;
-    perimeter = 2 * ( length + width);
-    printf(" Area: %f\n Perimeter %f",area,perimeter);
-    return 0;
- }
