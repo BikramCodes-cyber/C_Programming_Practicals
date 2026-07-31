39e01f593411207fe9cdfc733fdd88a9076d41d1
BikramCodes-cyber <kumarbikram13924@gmail.com>
2026-07-31 21:41:40 +0530

    Update q17.c

--- q17.c
+++ q17.c
@@ -1,3 +1,10 @@
+/*
+ * File Name: q17.c
+ * Program: Sum and Product of Two Numbers
+ * Description: Accepts two integers from the user and displays their sum and product.
+ * Author: Bikram
+ */
+
 #include<stdio.h>
 int main ()
 {
