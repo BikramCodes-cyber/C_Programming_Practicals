364e450bdb50fd729ad3010f4f0fa2e8f48f5abb
BikramCodes-cyber <kumarbikram13924@gmail.com>
2026-08-04 19:28:47 +0530

    Update q25.c

--- q25.c
+++ q25.c
@@ -1,3 +1,10 @@
+/*
+ * File Name: q25.c
+ * Program: Pass or Fail
+ * Description: This program checks whether a student has passed or failed based on the entered marks using if-else statements.
+ * Author: Bikram
+ */
+
 #include<stdio.h>
 int main ()
 {
